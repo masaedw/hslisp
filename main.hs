259cefa49ae3fa7d28f21620ffb686ba8269fe90
masaedw <masa.edw@gmail.com>
2011-10-10 10:56:15 +0900

    print parse result

--- main.hs
+++ main.hs
@@ -1,4 +1,24 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Control.Monad.IO.Class (liftIO)
+import Data.Attoparsec.Enumerator
+import Data.Enumerator (($$), (<==<), ($=), (=$))
+import Data.Enumerator.Binary as EB
+import System
+import qualified Data.ByteString as BS
+import qualified Data.Enumerator as E
+import qualified Data.Enumerator.List as EL
+
+import Parse
+
+x :: Monad m => E.Iteratee BS.ByteString m [SExp]
+x = iterParser exprs
+
 main :: IO ()
 main = do
-  cs <- getContents
-  
+  args <- getArgs
+  let file = EB.enumFile $ Prelude.head args
+  -- E.run_ $ file $$ EL.mapM_ $ BS.putStrLn
+  hoge <- E.run_ $ file $$ x
+  Prelude.mapM_ print hoge
+  return ()
